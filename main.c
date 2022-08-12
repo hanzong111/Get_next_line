be7220ec987c4090b3e12336d47b5ed6ccf4463f
Ong Jing Hanz <hanzong111@gmail.com>
2022-08-12 19:00:24 +0800

    Done but have timeout prob & havent done norm norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ojing-ha <ojing-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 17:16:38 by ojing-ha          #+#    #+#             */
-/*   Updated: 2022/08/06 14:01:49 by ojing-ha         ###   ########.fr       */
+/*   Updated: 2022/08/07 03:18:31 by ojing-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,16 @@ int     main(void)
 {
     FILE *fp = fopen("blank.txt", "r");
     int     fd = fileno(fp);
-	FILE *fp2 = fopen("blank2.txt", "r");
-    int     fd2 = fileno(fp2);
+	// FILE *fp2 = fopen("blank2.txt", "r");
+    // int     fd2 = fileno(fp2);
 	char	*str;
 
 	str = get_next_line(fd);
 	printf("Line 1 is %s\n", str);
 	free(str);
-	str = get_next_line(fd2);
-	printf("After everything the Finalstr is %s\n", str);
-	free(str);
+	// str = get_next_line(fd2);
+	// printf("After everything the Finalstr is %s\n", str);
+	// free(str);
 	str = get_next_line(fd);
 	printf("Line 2 is %s\n", str);
 	free(str);
@@ -38,8 +38,8 @@ int     main(void)
 	str = get_next_line(fd);
 	printf("Line 4 is %s\n", str);
 	free(str);
-	str = get_next_line(fd2);
-	printf("After everything the Finalstr is %s\n", str);
-	free(str);
+	// str = get_next_line(fd2);
+	// printf("After everything the Finalstr is %s\n", str);
+	// free(str);
     return (0);
 }
